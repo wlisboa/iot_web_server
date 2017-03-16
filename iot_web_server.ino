085c0da964218f1a2566d7f7c839e7500a225ced
Wahington Pinto Lisboa <washington.lisboa@fpf.br>
2017-03-16 16:49:30 -0400

    inclusão de comentários

--- iot_web_server.ino
+++ iot_web_server.ino
@@ -8,13 +8,27 @@ const char *password = "12345678";
 ESP8266WebServer server(80);
 
 const int led = 13;
-
+/*
+ *=============================================================================
+ *NAME:         void handleRoot()
+ *
+ *DESCRIPTION:  Essa função é usada pelo servidor web e é responsavel por
+ *              enviar a mensagem de hello
+ *=============================================================================
+*/
 void handleRoot() {
   digitalWrite(led, 1);
   server.send(200, "text/plain", "hello from esp8266!");
   digitalWrite(led, 0);
 }
-
+/*
+ *=============================================================================
+ *NAME:         handleNotFound(){
+ *
+ *DESCRIPTION:  Essa função é usada pelo servidor web e é responsavel por
+ *              enviar uma página de erro.
+ *=============================================================================
+*/
 void handleNotFound(){
   digitalWrite(led, 1);
   String message = "File Not Found\n\n";
@@ -31,7 +45,14 @@ void handleNotFound(){
   server.send(404, "text/plain", message);
   digitalWrite(led, 0);
 }
-
+/*
+ *=============================================================================
+ *NAME:         void setup(void){
+ *
+ *DESCRIPTION:  Essa é a função de inicialização do sistema, nessa função
+ *              é feita a configuração de todoo hardware que será utilizado
+ *=============================================================================
+*/
 void setup(void){
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
@@ -59,7 +80,14 @@ void setup(void){
   server.begin();
   Serial.println("HTTP server started");
 }
-
+/*
+ *=============================================================================
+ *NAME:         void loop(void){
+ *
+ *DESCRIPTION:  Essa é a função a função principal, essa função é chamada
+ *              repetidamente. 
+ *=============================================================================
+*/
 void loop(void){
   server.handleClient();
 }
